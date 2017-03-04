2a5e2cba63dd733cb01bf1f966d62811c8807f1f
spacemonkeythe <mladen.meseldzija@gmail.com>
2017-03-04 15:37:33 +0100

    Define prop types for components.

--- app/KanbanBoard.js
+++ app/KanbanBoard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import List from './List';
 
 class KanbanBoard extends Component {
@@ -23,6 +23,10 @@ class KanbanBoard extends Component {
       </div>
     );
   }
+};
+
+KanbanBoard.propTypes = {
+    cards: PropTypes.arrayOf(PropTypes.object)
 }
 
 export default KanbanBoard;
--- app/List.js
+++ app/List.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Card from './Card';
 
 class List extends Component {
@@ -22,5 +22,10 @@ class List extends Component {
     );
   }
 }
+
+List.propTypes = {
+  title: PropTypes.string.isRequired,
+  cards: PropTypes.arrayOf(PropTypes.object)
+};
 
 export default List;
